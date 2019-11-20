4331572d836f4632b8219ca56546cd73ab37b7f6
CedricSOEDJEDE <mawuyram-frido.soedjede@etu-univ-coteazur.fr>
2019-11-20 09:29:48 +0100

    Adding tests files

---
+++ tests/BaseTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testSomething()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(true, 'This should already work.');
+    }
+}
+?>
